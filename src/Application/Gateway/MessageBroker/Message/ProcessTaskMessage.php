ac19c7facc9bcf4ead767299889269af9ff2343e
alex <ssokoloss1@gmail.com>
2026-06-25 00:35:03 +0300

    refactored

--- src/Application/Gateway/MessageBroker/Message/ProcessTaskMessage.php
+++ src/Application/Gateway/MessageBroker/Message/ProcessTaskMessage.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Application\Gateway\MessageBroker\Message;
 
+use App\Infrastructure\MessageBroker\Amqp\Topology;
+
 final readonly class ProcessTaskMessage implements MessageInterface
 {
     public function __construct(
@@ -14,6 +16,6 @@ final readonly class ProcessTaskMessage implements MessageInterface
 
     public function getRoutingKey(): string
     {
-        return 'task.process';
+        return Topology::ROUTING_KEY;
     }
 }
